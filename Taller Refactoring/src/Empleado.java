a0a0b9d7a7a3bcd9cfd06fdd0baaae5a86429246
mcpoveda2 <mcpoveda@espol.edu.ec>
2024-07-31 16:44:36 -0500

    Pull up Refactoring by Michael poveda

--- Taller Refactoring/src/Empleado.java
+++ Taller Refactoring/src/Empleado.java
@@ -4,14 +4,17 @@ public class Empleado {
     private int horasTrabajadas;
     private String departamento;
     private double tarifaHora;
-
+    private String genero;
+    
     public Empleado(){}
-    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
+    
+    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento, String genero) {
         this.nombre = nombre;
         this.salarioBase = salarioBase;
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaHora = tarifaHora;
         this.departamento = departamento;
+        this.genero = genero;
     }
 
     public double calcularSalario() {
@@ -43,6 +46,9 @@ public class Empleado {
 
     public String getNombre() {
         return nombre;
+    }
+    public String getGenero() {
+    	return genero;
     }
 
     public void setNombre(String nombre) {
